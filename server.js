5a58d7be167cc9f739950433112614d630149e02
rramadhana <119663465+rramadhana@users.noreply.github.com>
2023-05-10 20:27:22 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('deployment');
+	res.send('deployment tcc clear');
 });
 
 const PORT = process.env.PORT || 8080;

fc2e2443f026869b3c434acc792ccae83620dda9
rramadhana <119663465+rramadhana@users.noreply.github.com>
2023-05-10 20:25:22 +0700

    update bro

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('testing brodie');
+	res.send('testing mantap');
 });
 
 const PORT = process.env.PORT || 8080;
